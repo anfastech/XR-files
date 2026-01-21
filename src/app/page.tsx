2c7cc67b97d0327bbe199956b3441d83e7fcb90a
anfastech <kpanfas6@gmail.com>
2026-01-21 18:27:43 +0530

    Fix: initial commit

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TodoForm from "@/components/TodoForm";
-
-export default function Home() {
-  return (
-    <main className="w-full min-h-screen bg-slate-950 text-slate-200">
-      <TodoForm />
-    </main>
-  );
-}
